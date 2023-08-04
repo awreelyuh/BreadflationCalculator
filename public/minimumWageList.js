f4e884bc5e0f879437674c3fcc6d1a4bbbc16070
Aurelia Ang <findaurelia@live.com>
2023-08-04 11:45:39 -0400

    removed dollar sign from minimum wage value

--- public/minimumWageList.js
+++ public/minimumWageList.js
@@ -1,179 +1,179 @@
 const minimumWage = [
     {
         "Year": "1980",
-        "FederalMinimumWage": "$3.10"
+        "FederalMinimumWage": "3.10"
     },
     {
         "Year": "1981",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1982",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1983",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1984",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1985",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1986",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1987",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1988",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1989",
-        "FederalMinimumWage": "$3.35"
+        "FederalMinimumWage": "3.35"
     },
     {
         "Year": "1990",
-        "FederalMinimumWage": "$3.80"
+        "FederalMinimumWage": "3.80"
     },
     {
         "Year": "1991",
-        "FederalMinimumWage": "$4.25"
+        "FederalMinimumWage": "4.25"
     },
     {
         "Year": "1992",
-        "FederalMinimumWage": "$4.25"
+        "FederalMinimumWage": "4.25"
     },
     {
         "Year": "1993",
-        "FederalMinimumWage": "$4.25"
+        "FederalMinimumWage": "4.25"
     },
     {
         "Year": "1994",
-        "FederalMinimumWage": "$4.25"
+        "FederalMinimumWage": "4.25"
     },
     {
         "Year": "1995",
-        "FederalMinimumWage": "$4.25"
+        "FederalMinimumWage": "4.25"
     },
     {
         "Year": "1996",
-        "FederalMinimumWage": "$4.75"
+        "FederalMinimumWage": "4.75"
     },
     {
         "Year": "1997",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "1998",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "1999",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "2000",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "2001",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "2002",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "2003",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "2004",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "2005",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "2006",
-        "FederalMinimumWage": "$5.15"
+        "FederalMinimumWage": "5.15"
     },
     {
         "Year": "2007",
-        "FederalMinimumWage": "$5.85"
+        "FederalMinimumWage": "5.85"
     },
     {
         "Year": "2008",
-        "FederalMinimumWage": "$6.55"
+        "FederalMinimumWage": "6.55"
     },
     {
         "Year": "2009",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2010",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2011",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2012",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2013",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2014",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2015",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2016",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2017",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2018",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2019",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2020",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2021",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2022",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     },
     {
         "Year": "2023",
-        "FederalMinimumWage": "$7.25"
+        "FederalMinimumWage": "7.25"
     }
 ];
 
