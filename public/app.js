5f1135b3ead386ad6eeb28ac62e910ac9dffc8ee
Aurelia Ang <findaurelia@live.com>
2023-08-04 10:00:08 -0400

    refactored and moved api variables

--- public/app.js
+++ public/app.js
@@ -1,13 +1,16 @@
 'use strict'
 
-const blsApiKey = '1baef04384504c8bb691c2a06e542dd9';
-const blsApiUrl = 'https://api.bls.gov/publicAPI/v2/timeseries/data/APU0000702111?registrationkey=';
-
 const userWageButton = document.getElementById('submit--latest');
 userWageButton.addEventListener('click', onWageSubmit, false);
 
 const dateButton = document.getElementById('submit--historical');
 dateButton.addEventListener('click', onDateSubmit, false);
+
+const blsApiKey = '1baef04384504c8bb691c2a06e542dd9';
+const blsApiUrl = `https://api.bls.gov/publicAPI/v2/timeseries/data/APU0000702111?registrationkey=${blsApiKey}`;
+
+const fredApiKey = 'b681ac4182da99f6769bab0d9b2c01da';
+const fredApiUrl = `https://api.stlouisfed.org/fred/series/observations?file_type=json&series_id=FEDMINNFRWG&observation_start=1980-01-01&api_key=${fredApiKey}`;
 
 //Calculation based on latest average price of white bread in a US city
 function onWageSubmit() {
@@ -15,7 +18,7 @@ function onWageSubmit() {
     const latestPrice = document.getElementById('price-of-bread--latest');
     const calculatedLoaves = document.getElementById('user-loaves');
 
-    fetch(blsApiUrl + blsApiKey + '&latest=true')
+    fetch(blsApiUrl + '&latest=true')
         .then(response => {
             if (!response.ok) {
                 console.error(error);
@@ -41,7 +44,7 @@ function onDateSubmit() {
     const histCalculatedLoaves = document.getElementById('historical-loaves');
     const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-    fetch(blsApiUrl + blsApiKey)
+    fetch(blsApiUrl)
         .then(response => {
             if (!response.ok) {
                 console.error(error);
@@ -51,13 +54,13 @@ function onDateSubmit() {
                     const dateInput = convertDate(date);
                     // console.log("Month name: " + monthNames[dateInput.getMonth()]);
                     // console.log("Year: " + dateInput.getFullYear());                    
-                    fetch(blsApiUrl + blsApiKey + `&startyear=${dateInput.getFullYear()}` + `&endyear=${dateInput.getFullYear()}`)
+                    fetch(blsApiUrl + `&startyear=${dateInput.getFullYear()}` + `&endyear=${dateInput.getFullYear()}`)
                         .then(response => {
                             response.json()
                                 .then(json => {
                                     const histJsonData = json['Results']['series'][0]['data'];
                                     const selectedData = histJsonData.filter((entry) => entry.periodName === monthNames[dateInput.getMonth()]);
-                                    
+
                                     const textForHistPrice = document.getElementById('text-price-bread--historical');
                                     textForHistPrice.innerHTML = `The average cost of a loaf of white bread in ${monthNames[dateInput.getMonth()]} ${dateInput.getFullYear()} was `
                                     historicalPrice.innerHTML = '$' + selectedData[0].value;
